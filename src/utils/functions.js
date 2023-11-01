21b38acafdf60dbc912b67055a5da8098124b5e6
KindDev813 <121258426+KindDev813@users.noreply.github.com>
2023-10-31 20:52:19 -0400

    feat: Fixed some issues

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -153,3 +153,11 @@ export const alertModal = (value) => {
     title: value,
   });
 };
+
+export const isEmpty = (obj) => {
+  if (obj === null || obj === undefined) return !obj;
+  else if (typeof obj === "string" || Array.isArray(obj))
+    return obj.length === 0;
+  else if (typeof obj === "object") return Object.keys(obj).length === 0;
+  else return !obj;
+};
